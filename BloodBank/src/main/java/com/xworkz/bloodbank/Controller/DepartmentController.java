5ad02f8313444cbb2e4c2d8653503d9c8cb6a6f9
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-08 23:32:24 +0530

    Controllers file helping move data from Runner to service integrating with Hibernate and JPA

---
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Controller/DepartmentController.java
@@ -0,0 +1,16 @@
+package com.xworkz.bloodbank.Controller;
+
+import com.xworkz.bloodbank.Dao.impl.DepartmentServiceImpl;
+import com.xworkz.bloodbank.Dto.DepartmentDto;
+import com.xworkz.bloodbank.service.DepartmentService;
+
+public class DepartmentController {
+
+    DepartmentService departmentService = new DepartmentServiceImpl();
+    public  boolean saveDepartment(DepartmentDto department){
+        return departmentService.saveDepartment(department);
+    }
+    public void getDepartment(){
+        departmentService.getDepartment();
+    }
+}
