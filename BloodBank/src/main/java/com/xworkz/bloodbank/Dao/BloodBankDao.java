b2a34be8edf7195650e54aa3d10b69d8967cc948
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-09 20:11:12 +0530

    Dao files are helping to connect to DB and integrating with Hibernate and JPA

---
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/BloodBankDao.java
@@ -0,0 +1,9 @@
+package com.xworkz.bloodbank.Dao;
+
+import com.xworkz.bloodbank.entity.BloodBankEntity;
+
+public interface BloodBankDao {
+
+    boolean saveBloodBank(BloodBankEntity bloodBank);
+    void getBloodbank();
+}
