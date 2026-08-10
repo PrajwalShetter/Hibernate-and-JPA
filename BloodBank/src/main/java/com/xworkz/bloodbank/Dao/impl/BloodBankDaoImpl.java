36f21112811794d9e0d32cafc84f75819950c997
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-10 23:53:51 +0530

    Integrating configuration file along with sessionFactory and session

--- BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/BloodBankDaoImpl.java
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/BloodBankDaoImpl.java
@@ -2,23 +2,36 @@ package com.xworkz.bloodbank.Dao.impl;
 
 import com.xworkz.bloodbank.Dao.BloodBankDao;
 import com.xworkz.bloodbank.entity.BloodBankEntity;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BloodBankDaoImpl implements BloodBankDao {
 
-    List<BloodBankEntity>  bloodBankEntities = new ArrayList<>();
+
     @Override
     public boolean saveBloodBank(BloodBankEntity bloodBank) {
-        return bloodBankEntities.add(bloodBank);
+
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.save(bloodBank);
+        session.close();
+        sessionFactory.close();
+
+        return true;
     }
 
     @Override
     public void getBloodbank() {
-        for (BloodBankEntity bloodBank : bloodBankEntities){
-            System.out.println(bloodBank);
-        }
+//        for (BloodBankEntity bloodBank : bloodBankEntities){
+//            System.out.println(bloodBank);
+//        }
 
     }
 }
