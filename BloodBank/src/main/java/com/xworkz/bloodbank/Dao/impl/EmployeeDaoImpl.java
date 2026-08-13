a34f151c57d833fee41ebf3489983444286eb91b
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-13 23:45:02 +0530

    Add configuration codes of hibernate and SessionFactory

--- BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/EmployeeDaoImpl.java
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/EmployeeDaoImpl.java
@@ -2,23 +2,44 @@ package com.xworkz.bloodbank.Dao.impl;
 
 import com.xworkz.bloodbank.Dao.EmployeeDao;
 import com.xworkz.bloodbank.entity.EmployeeEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDaoImpl implements EmployeeDao {
 
-    List<EmployeeEntity> employeeEntities = new ArrayList<>();
+
     @Override
     public boolean saveEmployee(EmployeeEntity employeeEntity) {
-        return employeeEntities.add(employeeEntity);
+
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(employeeEntity);
+
+        transaction.commit();
+
+        session.close();
+        sessionFactory.close();
+
+        return true;
     }
 
     @Override
     public void getEmployee() {
 
-        for(EmployeeEntity emp: employeeEntities){
-            System.out.println(emp);
-        }
+//        for(EmployeeEntity emp: employeeEntities){
+//            System.out.println(emp);
+//        }
     }
 }
