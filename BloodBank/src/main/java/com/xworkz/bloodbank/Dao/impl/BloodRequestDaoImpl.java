023cabc91afdf4d7e30b5312316263bf1b089be4
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-13 23:43:39 +0530

    Add configuration codes of hibernate and SessionFactory

--- BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/BloodRequestDaoImpl.java
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/BloodRequestDaoImpl.java
@@ -2,23 +2,43 @@ package com.xworkz.bloodbank.Dao.impl;
 
 import com.xworkz.bloodbank.Dao.BloodRequestDao;
 import com.xworkz.bloodbank.entity.BloodRequestEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BloodRequestDaoImpl implements BloodRequestDao {
 
-    List<BloodRequestEntity> requestEntities = new ArrayList<>();
+
     @Override
     public boolean saveRequest(BloodRequestEntity requestEntity) {
-        return requestEntities.add(requestEntity);
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(requestEntity);
+
+        transaction.commit();
+
+        session.close();
+        sessionFactory.close();
+
+        return true;
     }
 
     @Override
     public void getRequest() {
-        for(BloodRequestEntity entity : requestEntities){
-            System.out.println(entity);
-        }
+//        for(BloodRequestEntity entity : requestEntities){
+//            System.out.println(entity);
+//        }
 
     }
 }
