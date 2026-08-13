2c0a19f4f30ceeec9d28da2352c3d4c9466f0c65
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-13 23:46:02 +0530

    Add configuration codes of hibernate and SessionFactory as well as performing Save and Get operations

--- BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/BloodBankDaoImpl.java
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/BloodBankDaoImpl.java
@@ -4,11 +4,22 @@ import com.xworkz.bloodbank.Dao.BloodBankDao;
 import com.xworkz.bloodbank.entity.BloodBankEntity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BloodBankDaoImpl implements BloodBankDao {
+
+    private Configuration configuration = new Configuration();
+    private SessionFactory sessionFactory;
+
+    public BloodBankDaoImpl(){
+        configuration.configure();
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
 
 
     @Override
@@ -20,7 +31,13 @@ public class BloodBankDaoImpl implements BloodBankDao {
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
         session.save(bloodBank);
+
+        transaction.commit();
+
         session.close();
         sessionFactory.close();
 
@@ -29,9 +46,27 @@ public class BloodBankDaoImpl implements BloodBankDao {
 
     @Override
     public void getBloodbank() {
-//        for (BloodBankEntity bloodBank : bloodBankEntities){
-//            System.out.println(bloodBank);
-//        }
 
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        List<BloodBankEntity> result = session.createQuery("from BloodBankEntity", BloodBankEntity.class).getResultList();
+        for(BloodBankEntity bloodBank : result){
+            System.out.println(bloodBank);
+        }
+        session.close();
+        sessionFactory.close();
+    }
+
+    @Override
+    public BloodBankEntity getById(int id) {
+
+        Session session = sessionFactory.openSession();
+
+        BloodBankEntity bloodBank = session.get(BloodBankEntity.class,id);
+        session.close();
+        return bloodBank;
     }
 }
