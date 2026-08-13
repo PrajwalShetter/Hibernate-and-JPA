b72182b38accdf2e27274af8baae9d0525736e94
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-13 23:45:13 +0530

    Add configuration codes of hibernate and SessionFactory

--- BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/PatientDaoImpl.java
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/PatientDaoImpl.java
@@ -2,23 +2,42 @@ package com.xworkz.bloodbank.Dao.impl;
 
 import com.xworkz.bloodbank.Dao.PatientDao;
 import com.xworkz.bloodbank.entity.PatientEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PatientDaoImpl implements PatientDao {
 
-    List<PatientEntity> patientEntities = new ArrayList<>();
+
     @Override
     public boolean savePatient(PatientEntity patient) {
 
-        return patientEntities.add(patient);
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(patient);
+
+        transaction.commit();
+
+        session.close();
+        sessionFactory.close();
+
+        return true;
     }
 
     @Override
     public void getPatient() {
-            for(PatientEntity patient:patientEntities){
-                System.out.println(patient);
-            }
+//            for(PatientEntity patient:patientEntities){
+//                System.out.println(patient);
+//            }
     }
 }
