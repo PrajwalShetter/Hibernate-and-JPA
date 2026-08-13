77b93632f81fc27691a44cc8f3d0ba8e85514c8a
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-13 23:44:23 +0530

    Add configuration codes of hibernate and SessionFactory

--- BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/DepartmentDaoImpl.java
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/DepartmentDaoImpl.java
@@ -2,24 +2,43 @@ package com.xworkz.bloodbank.Dao.impl;
 
 import com.xworkz.bloodbank.Dao.DepartmentDao;
 import com.xworkz.bloodbank.entity.DepartmentEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDaoImpl implements DepartmentDao {
 
-    List<DepartmentEntity> departmentEntities= new ArrayList<>();
+
     @Override
     public boolean saveDepartment(DepartmentEntity departmentEntity) {
-        return departmentEntities.add(departmentEntity);
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(departmentEntity);
+
+        transaction.commit();
+
+        session.close();
+        sessionFactory.close();
+
+        return true;
     }
 
     @Override
     public void getDepartment() {
 
-        for (DepartmentEntity dpt: departmentEntities){
-            System.out.println(dpt);
-        }
+//        for (DepartmentEntity dpt: departmentEntities){
+//            System.out.println(dpt);
+//        }
 
     }
 }
