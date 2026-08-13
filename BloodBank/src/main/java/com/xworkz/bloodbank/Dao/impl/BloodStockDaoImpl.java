f7107f92dfbef4362ebb72b96ee2c89b74394b9d
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-13 23:44:05 +0530

    Add configuration codes of hibernate and SessionFactory

--- BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/BloodStockDaoImpl.java
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/BloodStockDaoImpl.java
@@ -2,23 +2,42 @@ package com.xworkz.bloodbank.Dao.impl;
 
 import com.xworkz.bloodbank.Dao.BloodStockDao;
 import com.xworkz.bloodbank.entity.BloodStockEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BloodStockDaoImpl implements BloodStockDao {
 
-    List<BloodStockEntity> bloodStockEntities = new ArrayList<>();
+
     @Override
     public boolean saveBloodStock(BloodStockEntity bloodStockEntity) {
-      return bloodStockEntities.add(bloodStockEntity);
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(bloodStockEntity);
+
+        transaction.commit();
+
+        session.close();
+        sessionFactory.close();
+
+        return true;
     }
 
     @Override
     public void getBloodStock() {
 
-        for (BloodStockEntity stock : bloodStockEntities){
-            System.out.println(stock);
-        }
+//        for (BloodStockEntity stock : bloodStockEntities){
+//            System.out.println(stock);
+//        }
     }
 }
