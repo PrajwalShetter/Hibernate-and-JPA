076fedfc0706e29cb8df4cde1d623942e7fd1a88
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-13 23:44:49 +0530

    Add configuration codes of hibernate and SessionFactory

--- BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/DonorDaoImpl.java
+++ BloodBank/src/main/java/com/xworkz/bloodbank/Dao/impl/DonorDaoImpl.java
@@ -3,24 +3,41 @@ package com.xworkz.bloodbank.Dao.impl;
 import com.xworkz.bloodbank.Dao.DonorDao;
 import com.xworkz.bloodbank.entity.DonorEntity;
 import com.xworkz.bloodbank.service.DonorService;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DonorDaoImpl implements DonorDao {
 
-    List<DonorEntity> entities = new ArrayList<>();
+
     @Override
     public boolean saveDonor(DonorEntity donor) {
 
-        return entities.add(donor);
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(donor);
+
+        transaction.commit();
+
+        session.close();
+        sessionFactory.close();
+
+        return true;
     }
 
     @Override
     public void getDonors() {
-        for (DonorEntity dEntity: entities){
-            System.out.println(dEntity);
-        }
+
 
     }
 }
