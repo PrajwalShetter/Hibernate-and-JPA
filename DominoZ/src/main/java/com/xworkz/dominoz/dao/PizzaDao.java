806e67570cac6f124c45953ea0df8c345bb8c4cf
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-24 23:13:56 +0530

    DAO files responsible for Database connection and other resources , performed CRUD operation

---
+++ DominoZ/src/main/java/com/xworkz/dominoz/dao/PizzaDao.java
@@ -0,0 +1,13 @@
+package com.xworkz.dominoz.dao;
+
+import com.xworkz.dominoz.entity.PizzaEntity;
+
+import java.util.List;
+
+public interface PizzaDao {
+
+    boolean savePizza(PizzaEntity pizzaEntity);
+   PizzaEntity getPizza(int id);
+   boolean updatePrizeById(int id, double prize);
+   boolean deletePizzaById(int id);
+}
---
+++ DominoZ/src/main/java/com/xworkz/dominoz/dao/impl/PizzaDaoImpl.java
@@ -0,0 +1,81 @@
+package com.xworkz.dominoz.dao.impl;
+
+import com.xworkz.dominoz.dao.PizzaDao;
+import com.xworkz.dominoz.entity.PizzaEntity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.security.auth.login.Configuration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PizzaDaoImpl implements PizzaDao {
+
+    private  EntityManagerFactory entityManagerFactory;
+    private  EntityManager entityManager;
+
+
+    public  PizzaDaoImpl(){
+        entityManagerFactory= Persistence.createEntityManagerFactory("dominoz");
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @Override
+    public boolean savePizza(PizzaEntity pizzaEntity) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("dominoz");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        System.out.println(pizzaEntity.getPrice());
+        entityManager.getTransaction().begin();
+        entityManager.persist(pizzaEntity);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+            return true;
+    }
+
+    @Override
+    public PizzaEntity getPizza(int id) {
+       PizzaEntity pizzaEntity = entityManager.find(PizzaEntity.class,id);
+
+       if(pizzaEntity == null){
+           System.out.println("Id is not found");
+       }
+       entityManager.close();
+       return pizzaEntity;
+
+    }
+
+    @Override
+    public boolean updatePrizeById(int id, double prize) {
+
+        entityManager.getTransaction().begin();
+        PizzaEntity pizzaEntity= entityManager.find(PizzaEntity.class,id);
+        pizzaEntity.setPrice(prize);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return true;
+    }
+
+    @Override
+    public boolean deletePizzaById(int id) {
+
+        entityManager.getTransaction().begin();
+        PizzaEntity pizzaEntity = entityManager.find(PizzaEntity.class,id);
+        entityManager.remove(pizzaEntity);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+        return true;
+    }
+}
+
+
+
+
